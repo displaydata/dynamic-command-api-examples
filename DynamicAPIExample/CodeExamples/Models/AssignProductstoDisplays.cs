1ddbbdefe6f7356073caf58f9e841421147bf13a
Adam Wright <adam.wright@displaydata.com>
2019-06-27 12:22:56 +0100

    Created supporting API objects

---
+++ DynamicAPIExample/CodeExamples/Models/AssignProductstoDisplays.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeExamples
+{
+    public class ObjectSequence
+    {
+        public string ObjectId { get; set; }
+
+        public int Sequence { get; set; }
+    }
+
+    public class SearchValueObject
+    {
+        public string SearchValue { get; set; }
+
+        public int Sequence { get; set; }
+    }
+
+    public class SearchValuesObject
+    {
+        public IEnumerable<SearchValueObject> SearchValues { get; set; }
+        
+    }
+}
